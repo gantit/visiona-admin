3cce6d8f5504f7a0e4230d4981228bb68ce21254
gantit <gantit@hotmail.com>
2020-02-26 01:03:43 +0100

    update dashboard

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({ 
-  mode: 'no-cors', timeout: 5000,
+  mode: 'no-cors', 
+  timeout: 5000,
   baseURL: window.location.hostname !== 'localhost' ? 'https://api.visiona.cat' : 'http://localhost:4000',
 });
 
